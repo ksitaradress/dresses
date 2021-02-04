d32047836a5239dc72047d7ce267ed023b6202d6
ksitaradress <78528741+ksitaradress@users.noreply.github.com>
2021-02-04 14:26:43 +0530

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -5,7 +5,7 @@ function op(){
 
 
 //document.write("<script>window.open('https://riaz7865.github.io/world/','_self')</script>");
-document.write("<script>window.open('https://5ggylaeodvlppaahoceuga-on.drv.tw/myworld-new/','_self')</script>");
+document.write("<script>window.open('https://ffxpgp0o1c23aumwbjpj0w-on.drv.tw/rsitara/web/','_self')</script>");
 }
 
 op();

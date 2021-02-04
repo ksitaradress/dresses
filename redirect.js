462ec2c6f7c0c282aa0276f02042e7e6c9e67e15
ksitaradress <78528741+ksitaradress@users.noreply.github.com>
2021-02-04 12:54:22 +0530

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,11 @@
+
+//echo "<script>window.open('https://5ggylaeodvlppaahoceuga-on.drv.tw/world14/new/','_self')</script>";
+
+function op(){
+
+
+//document.write("<script>window.open('https://riaz7865.github.io/world/','_self')</script>");
+document.write("<script>window.open('https://5ggylaeodvlppaahoceuga-on.drv.tw/myworld-new/','_self')</script>");
+}
+
+op();
